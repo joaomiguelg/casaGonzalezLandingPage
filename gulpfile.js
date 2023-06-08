1e9d91ca159245921a9f392be21de57482a3b8ae
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2023-06-07 22:19:32 -0300

    teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const compImg = require('gulp-imagemin');
 
 function defaultTask(cb) {
     console.log('ola')
@@ -18,10 +19,16 @@ function buildStyles() {
 };
 
 
+function compImage (){
+    return gulp.src('./src/images/*')
+    .pipe(compImg())
+    .pipe(gulp.dest('./build/images'));
+}
+
 exports.default = defaultTask
 exports.sass = buildStyles;
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.series(buildStyles))
 }
-
+exports.images = compImage
 
